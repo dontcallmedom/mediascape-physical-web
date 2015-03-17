aa7228259c25a2b0c254758300e03d49927c3661
Dominique Hazael-Massieux <dom@w3.org>
2015-03-17 12:41:51 +0100

    use localhost for api backend

--- public/js/broadcastUrl-polyfill.js
+++ public/js/broadcastUrl-polyfill.js
@@ -1,14 +1,14 @@
 navigator.mediascapeBroadcastURL = function(url) {
     if (confirm("Do you want to share this app with nearby devices?")) {
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "/api/beacon");
+        xhr.open("POST", "http://localhost:3000/api/beacon");
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.send("action=start&url=" + encodeURIComponent(url));
     }
 
     return {stop: function () {
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", "/api/beacon");
+        xhr.open("POST", "http://localhost:3000/api/beacon");
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.send("action=stop");
     }};
