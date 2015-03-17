3c62956d2e8936016e134b1172f05b0c4ed2833e
Dominique Hazael-Massieux <dom@w3.org>
2015-03-17 12:41:38 +0100

    make share happen automatically

--- public/js/share.js
+++ public/js/share.js
@@ -1,3 +1,7 @@
-document.getElementById("share").addEventListener("click", function(ev) {
+/*document.getElementById("share").addEventListener("click", function(ev) {
   navigator.mediascapeBroadcastURL("http://first:3000/c");
-});
+});*/
+
+setTimeout(function() {
+  navigator.mediascapeBroadcastURL("http://myapp:3000/c");
+}, 1000);
