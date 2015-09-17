e46165f5d7176d3fdc2bcd14fe5885e79136e892
François Daoust <fd@tidoust.net>
2015-09-17 18:08:22 +0200

    Now using latest Presentation API polyfill
    This is a first attempt to integrate the Physical Web prototype with the
    latest version of the Presentation API polyfill, which also supports Chromecast
    devices and opening the presentation in a separate window.
    
    The code is untested (and thus wrong!) since uri-beacon is Lunix-only and
    I don't have the right environment here.

--- public/js/share.js
+++ public/js/share.js
@@ -1,8 +1,9 @@
-/*document.getElementById("share").addEventListener("click", function(ev) {
-  navigator.mediascapeBroadcastURL("http://first:3000/c");
-});*/
-
-setTimeout(function() {
-    var pSession =navigator.mediascapePresentation.startSession("http://myapp:3000/c");
-    console.log(pSession);
-}, 1000);
+document.getElementById('share').addEventListener('click', function () {
+  var presentationRequest = new w3cPresentationRequest('http://myapp:3000/c');
+  presentationRequest.start().then(function (connection) {
+    connection.onstatechange = function () {
+      console.log('presentation connection state changed to "' + connection.state + '"');
+    };
+    console.log(connection);
+  });
+});
